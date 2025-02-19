6f8dcd52d177b47c1db952a26551159b4cfe93eb
Scarlett <154524564+ScarlettDuna@users.noreply.github.com>
2025-02-19 10:10:44 +0100

    no se habia subido bien

---
+++ CalcMinxkmAKph.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CalcMinxkmAKph {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int minutos, segundos;
+        System.out.println("Introduce los minutos");
+        minutos = sc.nextInt();
+        System.out.println("Introduce los segundos");
+        segundos = sc.nextInt();
+
+    }
+}
